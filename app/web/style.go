b8637d44fa8559e63b3d61fc0b9e0cb0574aa4d6
Alberto Cortés <alcortesm@gmail.com>
2020-09-04 11:49:23 +0200

    show JS graph of the utilization (#5)

--- app/web/style.go
+++ app/web/style.go
@@ -6,9 +6,23 @@ func StyleHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-type", "text/css")
 
-		body := []byte(`h1 {
-color: #36a8e1;
+		body := []byte(`
+h1 {
+  color: #36a8e1;
+  text-align: center;
+}
+
+.container {
+  width:70vw;
+  margin:auto;
+  display:block;
+}
+
+canvas {
+  width:100%;
+  height:auto;
 }`)
+
 		w.Write(body)
 	})
 }
