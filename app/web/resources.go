464c110f409360676b17de34620d46b7f19783c1
Alberto Cortés <alcortesm@gmail.com>
2020-09-10 13:02:00 +0200

    visual improvements to the chart

--- app/web/resources.go
+++ app/web/resources.go
@@ -1,12 +1,7 @@
 package web
 
-const css = `h1 {
-  color: #36a8e1;
-  text-align: center;
-}
-
-.container {
-  width:70vw;
+const css = `.container {
+  width:80vw;
   margin:auto;
   display:block;
 }
@@ -21,35 +16,85 @@ const chartTemplate = `var ctx = document.getElementById('chart').getContext('2d
 const data = {{.People}};
 
 var chart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    datasets: [{
-      label: 'People',
-      data: data
-    }]
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        type: 'time'
-      }]
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'People',
+            data: data,
+            backgroundColor: '#36a8e1',
+            borderColor: 'black',
+            yAxisID: 'people'
+        }]
+    },
+    options: {
+        padding: 10,
+        title: {
+            text: "Sputnik",
+            display: true,
+            fontColor: '#36a8e1',
+            fontSize: 20
+        },
+        elements: {
+            line: { tension: 0 },
+        },
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    unit: 'day'
+                }
+            }],
+            yAxes: [{
+                id: 'people',
+                position: 'left',
+                scaleLabel: {
+                    display: true,
+                    labelString: 'people'
+                }
+            },
+            {
+                id: 'percent',
+                position: 'right',
+                scaleLabel: {
+                    display: true,
+                    labelString: 'percent'
+                }
+            }]
+        },
+        tooltips: {
+            callbacks: {
+                title: function (tooltipItem, data) {
+                    var raw = tooltipItem[0].xLabel;
+                    var date = new Date(raw);
+
+                    var formatter = new Intl.DateTimeFormat('en-us', {
+                        weekday: 'long',
+                        month: 'short',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        hour12: false
+                    });
+
+                    var result = formatter.format(date);
+
+                    return result;
+                },
+            }
+        }
     }
-  }
-});
-`
+});`
 
 const popularity = `<!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="utf-8">
-  <title>title</title>
+  <title>Sputnik Popularity</title>
   <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
-
-  <h1>Sputnik Popularity</h1>
 
   <div class="container">
     <canvas id="chart"></canvas>
@@ -60,19 +105,4 @@ const popularity = `<!DOCTYPE html>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
 <script src="./chart.js"></script>
 
-</html>`
-
-const noData = `<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Hello World</title>
-	<link rel="stylesheet" href="/style.css">
-</head>
-<body>
-
-	<h1>Sputnik Popularity</h1>
-	<p>There are no data available.</p>
-
-</body>
 </html>`
